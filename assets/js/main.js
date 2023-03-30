735e6c382f8a5cc2f607068010cc623a981ea799
Saqib Islam <saaqi.grw@gmail.com>
2023-03-30 14:55:11 +0500

    fixed some styling issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -181,3 +181,18 @@ form.addEventListener("submit", function(event) {
     // Prevent default form submission
     event.preventDefault();
 });
+
+
+
+
+
+
+
+/* ## HIDE PRELOADER
+--------------------------------------------- */
+// HIDE PRELOADER
+document.getElementById("atf-preloader").classList.add('preloader-hide');
+
+window.onload = setTimeout( function() {
+  document.querySelector('.preloader-hide').style.display = 'none';
+}, 1000);
